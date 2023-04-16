e22eddd45e3d7f93de48737967940a7ec837d0a2
ClementMathew <clementmathew924@gmail.com>
2023-04-16 08:11:46 +0530

    New

--- client_flutter/lib/Home/addDisaster.dart
+++ client_flutter/lib/Home/addDisaster.dart
@@ -1,3 +1,4 @@
+import 'package:client_flutter/Disaster/Addimage.dart';
 import 'package:flutter/material.dart';
 
 class AddDis extends StatefulWidget {
@@ -8,102 +9,247 @@ class AddDis extends StatefulWidget {
 }
 
 class _AddDisState extends State<AddDis> {
-  final tags = [
-    'Road Block',
-    'Landslide',
-    'Tiger',
-    'Downed powerline',
-    'Other',
-  ];
-  String? selectedLetter;
-
-  TextEditingController testName = TextEditingController();
-  TextEditingController testPhone = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Add Tags"),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 30, top: 20),
-            child: Row(
-              children: const [
-                Text("Select the type : ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-              ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Add Tags"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 30, top: 20, bottom: 20),
+              child: Row(
+                children: const [
+                  Text("Select the type : ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: DropdownButtonFormField(
-                decoration: InputDecoration(
-                  label: Text("Select from the dropdown"),
-                ),
-                borderRadius: BorderRadius.circular(20),
-                autofocus: true,
-                items: tags
-                    .map((e) => DropdownMenuItem(
-                          value: e,
-                          child: Text(e),
-                        ))
-                    .toList(),
-                onChanged: (value) {
-                  selectedLetter = value as String?;
-                }),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: OutlinedButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                    side: MaterialStateProperty.all(
-                        BorderSide(color: Colors.black, width: 3)),
-                    foregroundColor: MaterialStateProperty.all(Colors.black),
-                    minimumSize: MaterialStateProperty.all(Size(200, 50)),
-                    textStyle: MaterialStateProperty.all(
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 19))),
-                onPressed: () {},
-                child: Text("Add Images")),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(50, 15, 50, 0),
-            child: ElevatedButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30))),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                    minimumSize: MaterialStateProperty.all(Size(100, 50)),
-                    textStyle: MaterialStateProperty.all(TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ))),
-                onPressed: () {
-                  if (selectedLetter != null &&
-                      testName.text != "" &&
-                      testPhone.text != "") {
-                    Navigator.pop(context);
-                  }
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
+                  )),
+              child: ExpansionTile(
+                title: const Text('Pandemic',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                children: [
+                  // const Divider(
+                  //   color: Colors.black,
+                  //   thickness: .5,
+                  // ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddImage()));
+                    },
+                    child: const ListTile(
+                        title: Text(
+                      'Hepatitis-B',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20),
+                    )),
+                  ),
+                  const Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddImage()));
+                    },
+                    child: const ListTile(
+                        title: Text('Covid-19',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20))),
+                  ),
+                  const Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddImage()));
+                    },
+                    child: const ListTile(
+                        title: Text('Chickenpox',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  border: BorderDirectional(
+                      bottom: BorderSide(width: 2, color: Colors.black),
+                      end: BorderSide(width: 2, color: Colors.black),
+                      start: BorderSide(width: 2, color: Colors.black))),
+              child: ExpansionTile(
+                title: const Text('Animal Sightings',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddImage()));
+                    },
+                    child: const ListTile(
+                        title: Text('Tiger',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20))),
+                  ),
+                  const Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddImage()));
+                    },
+                    child: const ListTile(
+                        title: Text('Elephant',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
+            ),
+            InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddImage()));
                 },
-                child: Text("Submit")),
-          ),
-        ],
+                child: Container(
+                    height: 65,
+                    decoration: const BoxDecoration(
+                        border: BorderDirectional(
+                            bottom: BorderSide(width: 2, color: Colors.black),
+                            end: BorderSide(width: 2, color: Colors.black),
+                            start: BorderSide(width: 2, color: Colors.black))),
+                    child: Row(
+                      children: const [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Flood',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ))),
+            InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddImage()));
+                },
+                child: Container(
+                    height: 65,
+                    decoration: const BoxDecoration(
+                        border: BorderDirectional(
+                            bottom: BorderSide(width: 2, color: Colors.black),
+                            end: BorderSide(width: 2, color: Colors.black),
+                            start: BorderSide(width: 2, color: Colors.black))),
+                    child: Row(
+                      children: const [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Earthquake',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ))),
+            InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddImage()));
+                },
+                child: Container(
+                    height: 65,
+                    decoration: const BoxDecoration(
+                        border: BorderDirectional(
+                            bottom: BorderSide(width: 2, color: Colors.black),
+                            end: BorderSide(width: 2, color: Colors.black),
+                            start: BorderSide(width: 2, color: Colors.black))),
+                    child: Row(
+                      children: const [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Landslide',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ))),InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddImage()));
+                },
+                child: Container(
+                    height: 65,
+                    decoration: const BoxDecoration(
+                        border: BorderDirectional(
+                            bottom: BorderSide(width: 2, color: Colors.black),
+                            end: BorderSide(width: 2, color: Colors.black),
+                            start: BorderSide(width: 2, color: Colors.black))),
+                    child: Row(
+                      children: const [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Other',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )))
+          ],
+        ),
       ),
     );
   }
